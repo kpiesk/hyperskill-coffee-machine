c1609f367cc60ff623773e5c9f6f3868fd6e4aad
kpiesk <karolinaa1999@gmail.com>
2020-07-31 19:44:28 +0100

    Add Stage 3/6 project
    The program requests the amounts of water, milk, and coffee beans available at the moment, and then asks for the number of cups a user needs. Then it checks whether the coffee machine has enough supplies to make the specified amount of coffee.

--- Coffee Machine/coffee_machine.py
+++ Coffee Machine/coffee_machine.py
@@ -1,12 +1,29 @@
 water = 200  # in ml
 milk = 50  # in ml
-coffee_beans = 15  # in g
+beans = 15  # in g
 
+available_water = int(input(
+      'Write how many ml of water the coffee machine has: '))
+available_milk = int(input(
+      'Write how many ml of milk the coffee machine has: '))
+available_beans = int(input(
+      'Write how many grams of coffee beans the coffee machine has: '))
 cups = int(input('Write how many cups of coffee you will need: '))
-needed_water = cups * water
-needed_milk = cups * milk
-needed_coffee_beans = cups * coffee_beans
-print(f'For {cups} cups of coffee you wil need:'
-      f'\n{needed_water} ml of water'
-      f'\n{needed_milk} ml of milk'
-      f'\n{needed_coffee_beans} g of coffee beans')
+
+available_cups = 0
+while True:
+    available_water -= water
+    available_milk -= milk
+    available_beans -= beans
+    if available_water >= 0 and available_milk >= 0 and available_beans >= 0:
+        available_cups += 1
+    else:
+        break
+
+if cups == available_cups:
+    print('Yes, I can make that amount of coffee')
+elif available_cups > cups:
+    print(f'Yes, I can make that amount of coffee'
+          f' (and even {available_cups - cups} more than that)')
+else:
+    print(f'No, I can make only {available_cups} of coffee')
