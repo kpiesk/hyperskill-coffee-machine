897f822b0029f38a627f4c9d085eb35efe86b8f7
kpiesk <k.piesk@protonmail.com>
2020-08-08 20:04:24 +0100

    Add final stage project
    The program's functionality is the same, just now the program is implemented using class and methods.

--- Coffee Machine/coffee_machine.py
+++ Coffee Machine/coffee_machine.py
@@ -1,107 +1,106 @@
-BUY = 'buy'
-FILL = 'fill'
-TAKE = 'take'
-EXIT = 'exit'
-REMAINING = 'remaining'
+class CoffeeMachine:
+    BUY = 'buy'
+    FILL = 'fill'
+    TAKE = 'take'
+    EXIT = 'exit'
+    REMAINING = 'remaining'
+    BACK = 'back'
 
-water = 400
-milk = 540
-beans = 120
-cups = 9
-money = 550
+    ESPRESSO_CHOICE = '1'
+    LATTE_CHOICE = '2'
+    CAPPUCCINO_CHOICE = '3'
 
+    def __init__(self):
+        self.water = 400
+        self.milk = 540
+        self.beans = 120
+        self.cups = 9
+        self.money = 550
+        self.state = 'standby'
 
-def coffee_machine():
-    while True:
-        action = input('\nWrite action (buy, fill, take, remaining, exit): ')
-        if action == BUY:
-            buy()
-        elif action == FILL:
-            fill()
-        elif action == TAKE:
-            take()
-        elif action == REMAINING:
-            machine_status()
-        elif action == EXIT:
-            break
+    def main(self):
+        while True:
+            self.state = input('\nWrite action (buy, fill, take, remaining, exit): ')
+            if self.state == self.BUY:
+                self.buy()
+            elif self.state == self.FILL:
+                self.fill()
+            elif self.state == self.TAKE:
+                self.take()
+            elif self.state == self.REMAINING:
+                self.machine_status()
+            elif self.state == self.EXIT:
+                break
 
+    # prints current resources of the coffee machine
+    def machine_status(self):
+        print(f'\nThe coffee machine has'
+              f'\n{self.water} of water'
+              f'\n{self.milk} of milk'
+              f'\n{self.beans} of coffee beans'
+              f'\n{self.cups} of disposable cups'
+              f'\n{self.money} of money')
 
-# prints current resources of the coffee machine
-def machine_status():
-    print(f'\nThe coffee machine has'
-          f'\n{water} of water'
-          f'\n{milk} of milk'
-          f'\n{beans} of coffee beans'
-          f'\n{cups} of disposable cups'
-          f'\n{money} of money')
+    # allows the user to buy one of three types of coffee
+    def buy(self):
+        coffee_choice = input(
+            '\nWhat do you want to buy? '
+            '1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ')
+        if coffee_choice == self.ESPRESSO_CHOICE:
+            self.make_espresso()
+        elif coffee_choice == self.LATTE_CHOICE:
+            self.make_latte()
+        elif coffee_choice == self.CAPPUCCINO_CHOICE:
+            self.make_cappuccino()
+        elif coffee_choice == self.BACK:
+            return
 
+    # allows a special worker to fill out all the supplies
+    # for the coffee machine
+    def fill(self):
+        self.water += int(input(
+            '\nWrite how many ml of water do you want to add: '))
+        self.milk += int(input(
+            'Write how many ml of milk do you want to add: '))
+        self.beans += int(input(
+            'Write how many grams of coffee beans do you want to add: '))
+        self.cups += int(input(
+            'Write how many disposable cups of coffee do you want to add: '))
 
-# allows the user to buy one of three types of coffee
-def buy():
-    coffee_choice = input(
-        '\nWhat do you want to buy? '
-        '1 - espresso, 2 - latte, 3 - cappuccino, back - to min menu: ')
-    if coffee_choice == '1':
-        make_espresso()
-    elif coffee_choice == '2':
-        make_latte()
-    elif coffee_choice == '3':
-        make_cappuccino()
-    elif coffee_choice == 'back':
-        return
+    # allows a special worker to take out all of the money
+    # from the coffee machine
+    def take(self):
+        print(f'\nI gave you ${self.money}')
+        self.money = 0
 
+    # checks whether there are enough resources for making a coffee
+    # if there are, then makes the desired coffee
+    def make_coffee(self, needed_water, needed_milk, needed_beans, price):
+        if self.water < needed_water:
+            print('Sorry, not enough water')
+        elif self.milk < needed_milk:
+            print('Sorry, not enough milk')
+        elif self.beans < needed_beans:
+            print('Sorry, not enough coffee beans')
+        elif self.cups < 1:
+            print('Sorry, not enough disposable cups')
+        else:
+            self.water -= needed_water
+            self.milk -= needed_milk
+            self.beans -= needed_beans
+            self.cups -= 1
+            self.money += price
+            print('I have enough resources, making you a coffee!')
 
-# allows a special worker to fill out all the supplies for the coffee machine
-def fill():
-    global water, milk, beans, cups
-    water += int(input(
-        '\nWrite how many ml of water do you want to add: '))
-    milk += int(input(
-        'Write how many ml of milk do you want to add: '))
-    beans += int(input(
-        'Write how many grams of coffee beans do you want to add: '))
-    cups += int(input(
-        'Write how many disposable cups of coffee do you want to add: '))
+    def make_espresso(self):
+        self.make_coffee(needed_water=250, needed_milk=0, needed_beans=16, price=4)
 
+    def make_latte(self):
+        self.make_coffee(needed_water=350, needed_milk=75, needed_beans=20, price=7)
 
-# allows a special worker to take out all of the money from the coffee machine
-def take():
-    global money
-    print(f'\nI gave you ${money}')
-    money = 0
-
-
-# checks whether there are enough resources for making a coffee
-# if there are, then makes the desired coffee
-def make_coffee(needed_water, needed_milk, needed_beans, price):
-    global water, milk, beans, cups, money
-    if water < needed_water:
-        print('Sorry, not enough water')
-    elif milk < needed_milk:
-        print('Sorry, not enough milk')
-    elif beans < needed_beans:
-        print('Sorry, not enough coffee beans')
-    elif cups < 1:
-        print('Sorry, not enough disposable cups')
-    else:
-        water -= needed_water
-        milk -= needed_milk
-        beans -= needed_beans
-        cups -= 1
-        money += price
-        print('I have enough resources, making you a coffee!')
-
-
-def make_espresso():
-    make_coffee(needed_water=250, needed_milk=0, needed_beans=16, price=4)
-
-
-def make_latte():
-    make_coffee(needed_water=350, needed_milk=75, needed_beans=20, price=7)
-
-
-def make_cappuccino():
-    make_coffee(needed_water=200, needed_milk=100, needed_beans=12, price=6)
+    def make_cappuccino(self):
+        self.make_coffee(needed_water=200, needed_milk=100, needed_beans=12, price=6)
 
 
-coffee_machine()
+coffee_machine = CoffeeMachine()
+coffee_machine.main()
